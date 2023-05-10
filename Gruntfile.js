c5edd03cd6d610a8190e08a21eb634e274c90847
michel997 <gekhanom@outlook.com>
2023-05-10 20:29:44 -0300

    primeiro commit

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            dev: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    'dist/styles/main.css' : 'src/styles/main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js' : 'src/scripts/main.js'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['less','uglify']);
+};
